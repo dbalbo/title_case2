463820f3e38662e3bd94dd8c6bd4651b59f59779
steven francisco & debbie balbo <student@epicodus.com>
2015-05-27 12:11:51 -0700

    all specs and app is working

--- spec/title_case_integration_spec.rb
+++ spec/title_case_integration_spec.rb
@@ -1,1 +1,12 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
 
+describe('the title case path', {:type => :feature}) do
+  it("properly capitalizes a title") do
+    visit('/')
+    fill_in("title", :with => "Batman Returns")
+    click_button('send')
+    expect(page).to have_content ('Batman Returns')
+  end
+end
